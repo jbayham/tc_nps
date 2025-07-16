e42463be56a63bd49e82f4e437714b8523a91285
Jude Bayham <jbayham@colostate.edu>
2025-07-16 21:12:15 +0000

    commenting out tract and switching to zipcode

--- build/code/xx_tc_leg1.R
+++ build/code/xx_tc_leg1.R
@@ -4,16 +4,23 @@
 library(pacman)
 p_load(tidyverse,sf,janitor,measurements,progress,osrm,furrr,arrow,here)
 
-message("here() root: ", here::here())
-i_am("build/code/xx_tc_leg1.R")
 source(here("project_init.R"))
 
 # Read in data of all unique origins
-census_geo <- readRDS(here("build", "cache", "census_geo_points_2019.rds")) %>%
-  rename(tract = geoid)
+# census_geo <- readRDS(here("build", "cache", "census_geo_points_2019.rds")) %>%
+#   rename(tract = geoid)
+
+census_geo <- readRDS(here("build", "cache", "census_geo_zip_2024.rds")) %>%
+  rename(zip = geoid20)
 
 # Read in airports
 airports_geo <- read_csv(here("build", "cache", "airports_geo.csv"))
+
+###########
+#Start where left off
+#airports_geo = airports_geo[(which(airports_geo$code=="DAY")-1):nrow(airports_geo),]
+
+############
 
 
 if(interactive()){
@@ -28,13 +35,15 @@ if(interactive()){
 options(osrm.server = "http://darecompute-01.aggie.colostate.edu:5000/", osrm.profile = "car")
 
 # Break census tracts into chunks of 450
-census_chunks <- split(census_geo, ceiling(seq_along(census_geo$tract) / 450))
+#census_chunks <- split(census_geo, ceiling(seq_along(census_geo$tract) / 450))
+census_chunks <- split(census_geo, ceiling(seq_along(census_geo$zip) / 450))
 
-# airport_row=airports_geo[1,]
-# chunk=census_chunks[[1]]
-# i=1
+airport_row=airports_geo[1,]
+chunk=census_chunks[[1]]
+i=1
 
-dir_ifnot(here("build", "cache", "fly", "leg1"))
+#dir_ifnot(here("build", "cache", "fly", "tract_leg1"))
+dir_ifnot(here("build", "cache", "fly", "ztca_leg1"))
 
 # Function to calculate and cache OD matrix for one airport
 process_airport <- function(airport_row) {
@@ -50,7 +59,8 @@ process_airport <- function(airport_row) {
   
   future_walk2(census_chunks, seq_along(census_chunks), function(chunk, i) {
     chunk_id <- paste0("chunk_", sprintf("%03d", i))
-    out_path <- here("build", "cache", "fly", "leg1", paste0(airport_code, "_", chunk_id, ".parquet"))
+    # out_path <- here("build", "cache", "fly", "tract_leg1", paste0(airport_code, "_", chunk_id, ".parquet"))
+    out_path <- here("build", "cache", "fly", "ztca_leg1", paste0(airport_code, "_", chunk_id, ".parquet"))
     
     if (file.exists(out_path)) {
       message("Skipping cached file: ", out_path)
@@ -71,12 +81,14 @@ process_airport <- function(airport_row) {
     
     if (!is.null(result)) {
       dist_df <- tibble(
-        tract = chunk$tract,
+        #tract = chunk$tract,
+        zip = chunk$zip,
         airport = airport_code,
         duration_hr = conv_unit(as.vector(result$durations),from = "min",to = "hour"),
         distance_mi = conv_unit(as.vector(result$distances),from = "m", to = "mi")
       ) %>%
-        bind_cols(rename(result$sources,tract_lon=lon,tract_lat=lat)) %>%
+        #bind_cols(rename(result$sources,tract_lon=lon,tract_lat=lat)) %>%
+        bind_cols(rename(result$sources,zip_lon=lon,zip_lat=lat)) %>%
         add_column(airport_lon=result$destinations$lon,
                    airport_lat=result$destinations$lat)
       
